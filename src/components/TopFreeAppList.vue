0430b71b6989c01d1160e3037d950c47f9ece530
HeavenYuan <twheavenyuan@gmail.com>
2022-09-04 00:10:09 +0800

    chore(): Adjust the topFreeAppList layout

--- src/components/TopFreeAppList.vue
+++ src/components/TopFreeAppList.vue
@@ -1,23 +1,23 @@
 <template>
-  <a-list class="demo-loadmore-list" :loading="initLoading" item-layout="horizontal" :data-source="topFreeAppList">
+  <a-list class="demo-loadmore-list" :loading="initLoading" item-layout="vertical" split :data-source="topFreeAppList">
     <template #loadMore>
       <div v-if="!initLoading && !loading && !isCountLimit" :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }">
         <a-button @click="onLoadMore">loading more</a-button>
       </div>
     </template>
-    <template #renderItem="{ item }">
-      <a-list-item>
-        <a-skeleton avatar :title="false" :loading="!!item.loading" active>
-          <a-list-item-meta :description="item.category.attributes.label">
-            <template #title>
-              <p>{{ item['im:name'].label }}</p>
-            </template>
-            <template #avatar>
-              <a-avatar :src="item['im:image'][0].label" />
-            </template>
-          </a-list-item-meta>
-        </a-skeleton>
-      </a-list-item>
+    <template #renderItem="{ item, index }">
+      <li class="card">
+        <p>{{ index + 1 }}</p>
+        <a-avatar class="app-icon" :src="item['im:image'][0].label" />
+        <div class="content">
+          <p class="title">{{ item['im:name'].label }}</p>
+          <p class="desc">{{ item.category.attributes.label }}</p>
+          <div>
+            <a-rate :value="2" />
+            <span>(123)</span>
+          </div>
+        </div>
+      </li>
     </template>
   </a-list>
 </template>
@@ -36,7 +36,8 @@ const props = defineProps({
 const store = useStore()
 
 const count = ref(10)
-const isCountLimit = computed(() => count.value >= 100)
+const limit = 100
+const isCountLimit = computed(() => count.value >= limit)
 const initLoading = ref(false)
 const loading = ref(false)
 const topFreeAppList = computed(() => {
@@ -51,13 +52,84 @@ const topFreeAppList = computed(() => {
   return filter
 })
 
-const onLoadMore = () => count.value += 10
+const onLoadMore = () => (count.value += 10)
 
 onMounted(() => store.dispatch('data/getTopFreeAppList'))
 </script>
 
-<style scoped>
-.demo-loadmore-list {
-  min-height: 350px;
+<style lang="scss" scoped>
+$base_size_1: 96px;
+$base_size_2: 64px;
+
+.card {
+  display: flex;
+  align-items: center;
+  margin-bottom: 36px;
+
+  > p {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 10px;
+    font-size: 48px;
+    color: #ccc;
+  }
+
+  > .app-icon {
+    width: $base_size_1;
+    height: $base_size_1;
+    margin: 0 20px;
+  }
+
+  > .content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 65%;
+
+    p {
+      margin: 0;
+      white-space: nowrap;
+      overflow: hidden;
+      width: 100%;
+      text-align: start;
+    }
+
+    > .title {
+      font-size: 24px;
+      font-weight: bold;
+    }
+
+    > .desc {
+      color: #ccc;
+    }
+  }
+}
+
+@media screen and (max-width: 540px) {
+  .card {
+    margin-bottom: 28px;
+
+    > p {
+      margin: 0 5px;
+      font-size: 24px;
+    }
+
+    > .app-icon {
+      width: $base_size_2;
+      height: $base_size_2;
+      margin: 0 10px;
+    }
+
+    > .content {
+      > .title {
+        font-size: 18px;
+      }
+
+      > .desc {
+        font-size: 16px;
+      }
+    }
+  }
 }
 </style>
