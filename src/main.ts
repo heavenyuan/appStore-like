e17074f37261617f83e369b5003a9e93908e0d62
HeavenYuan <twheavenyuan@gmail.com>
2022-09-03 16:51:31 +0800

    chore(): Adjust ant-design-vue module

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import http from '@/axios/http'
 import api from '@/axios/api'
 import router from './router'
 import store from './store'
-import antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue/es';
+import 'ant-design-vue/dist/antd.css';
 
-const app = createApp(App).use(store).use(router).use(antd)
+const app = createApp(App).use(store).use(router).use(Antd)
 
 app.provide('http', http)
 app.provide('api', api)
